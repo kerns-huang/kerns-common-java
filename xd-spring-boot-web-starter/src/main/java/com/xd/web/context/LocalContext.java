45f3f157c41e7bb5faf2b4009dd336b2d60b155f
kk <realmall20@protonmail.com>
2019-12-10 20:21:25 +0800

    添加配置

---
+++ xd-spring-boot-web-starter/src/main/java/com/xd/web/context/LocalContext.java
@@ -0,0 +1,27 @@
+package com.xd.web.context;
+
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * 语言信息上下文
+ *
+ * @author xiaohei
+ * @create 2019-12-10 下午8:06
+ **/
+public class LocalContext {
+
+    private static ThreadLocal<Locale> threadLocal = new ThreadLocal<>();
+
+    public static Locale get(){
+        if(threadLocal.get()==null){
+            return Locale.ENGLISH;
+        }else{
+            return threadLocal.get();
+        }
+    }
+
+    public static void set(Locale locale){
+        threadLocal.set(locale);
+    }
+}
---
+++ xd-spring-boot-web-starter/src/main/java/com/xd/web/response/XdResponseBodyAdvisor.java
@@ -0,0 +1,39 @@
+package com.xd.web.response;
+
+import com.xd.core.util.StringUtils;
+import com.xd.core.web.context.RequestContext;
+import com.xd.core.web.response.Result;
+import com.xd.web.context.LocalContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * 返回结果处理，国际化信息的处理
+ *
+ * @author xiaohei
+ * @create 2019-12-10 下午7:58
+ **/
+@ControllerAdvice
+public class XdResponseBodyAdvisor implements ResponseBodyAdvice<Result> {
+    @Autowired
+    private MessageSource messageSource;
+    @Override
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+        return true;
+    }
+
+    @Override
+    public Result beforeBodyWrite(Result s, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+         if(StringUtils.isNotEmpty(s.getMsgCode())){
+            s.setMsg(messageSource.getMessage(s.getMsgCode(),null, LocalContext.get()));
+         }
+        return s;
+    }
+}
