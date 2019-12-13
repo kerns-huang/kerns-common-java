42adf217875d81f2ce5d33dc234123bf086f01fb
kk <realmall20@protonmail.com>
2019-12-13 17:40:44 +0800

    添加对spring-boots-mongo的强化

---
+++ xd-spring-boot-mongo-starter/src/main/java/com/xd/mongo/conf/MongoConfig.java
@@ -0,0 +1,35 @@
+package com.xd.mongo.conf;
+
+import com.xd.mongo.convert.BigDecimalConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.GenericConverter;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * mongo的配置信息
+ *
+ * @author xiaohei
+ * @create 2019-12-13 下午1:45
+ **/
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    MongoTransactionManager transactionManager(MongoDbFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+    @Bean
+    public MongoCustomConversions customConversions()
+    {
+        List<GenericConverter> converterList = new ArrayList<GenericConverter>();
+        converterList.add(new BigDecimalConverter());
+        return new MongoCustomConversions(converterList);
+    }
+}
