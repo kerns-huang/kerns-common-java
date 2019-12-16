7c7491b02bd7ac96bd23ca9fecd2ff7c3162475f
kk <realmall20@protonmail.com>
2019-12-16 11:28:03 +0800

    添加操作交集的操作

--- xd-spring-boot-redis-starter/src/main/java/com/xd/cache/CacheAdapter.java
+++ xd-spring-boot-redis-starter/src/main/java/com/xd/cache/CacheAdapter.java
@@ -1,13 +1,12 @@
 package com.xd.cache;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.CollectionUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -28,51 +27,80 @@ import java.util.stream.Collectors;
 public class CacheAdapter<T> {
     @Autowired
     private RedisTemplate<String, T> template;
-    public void setValue(String key,T value) {
-        template.opsForValue().set(key,value);
+
+    public void setValue(String key, T value) {
+        template.opsForValue().set(key, value);
     }
-    public void setValue(String key,T value, long timeOut) {
-        template.opsForValue().set(key,value,timeOut);
+
+    public void setValue(String key, T value, long timeOut) {
+        template.opsForValue().set(key, value, timeOut);
     }
+
     public void setValue(String key, T value, long timeOut, TimeUnit timeUnit) {
-        template.opsForValue().set(key,value,timeOut,timeUnit );
+        template.opsForValue().set(key, value, timeOut, timeUnit);
     }
-    public void getValue(String key){
+
+    public void getValue(String key) {
         template.opsForValue().get(key);
     }
 
     /**
      * 添加zset 数据
+     *
      * @param key
      * @param score
      * @param value
      */
-    public  Boolean zAdd(String key,Double score,T value){
-        return template.opsForZSet().add(key,value,score);
+    public Boolean zAdd(String key, Double score, T value) {
+        return template.opsForZSet().add(key, value, score);
     }
 
     /**
      * 按照分数获取数据
+     *
      * @param key
      * @param start
      * @param end
      * @return
      */
-    public  List<T> zRevRange(String key,Long start,Long end){
-        Set<ZSetOperations.TypedTuple<T>> set= template.opsForZSet().rangeWithScores(key,start,end);
-        return set.stream().map(a->a.getValue()).collect(Collectors.toList());
+    public List<T> zRevRange(String key, Long start, Long end) {
+        Set<ZSetOperations.TypedTuple<T>> set = template.opsForZSet().rangeWithScores(key, start, end);
+        return set.stream().map(a -> a.getValue()).collect(Collectors.toList());
     }
 
 
-    public Long sAdd(String key,T... values){
-        return template.opsForSet().add(key,values);
+    public Long sAdd(String key, T... values) {
+        return template.opsForSet().add(key, values);
     }
 
 
-    public Set<T> sMembers(String key){
+    public Set<T> sMembers(String key) {
         return template.opsForSet().members(key);
     }
 
+    /**
+     * 获取key的交集，生成新的集合
+     *
+     * @param newkey
+     * @param keys
+     * @return
+     */
+    public Long zInterstore(String newkey, List<String> keys) {
+        if (keys == null || keys.isEmpty()) {
+            return 0L;
+        }
+        if (keys.size() == 1) {
+            Set<ZSetOperations.TypedTuple<T>> set= template.opsForZSet().rangeWithScores(keys.get(0), 0, -1);
+            template.opsForZSet().add(newkey, set);
+            return Long.valueOf(set.size());
+        } else {
+            Set<String> set=new HashSet<>(keys.size()-1);
+            for(int i=1;i<keys.size();i++){
+                set.add(keys.get(i));
+            }
+            return template.opsForZSet().intersectAndStore(keys.get(0), set, newkey);
+        }
+    }
 
 
 }
