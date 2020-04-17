880a3b39d808c86a9ed3d490486cf76c6380d38f
kk <realmall20@protonmail.com>
2020-04-17 10:43:58 +0800

    m

--- xd-spring-boot-elasticsearch-starter/src/test/java/com/xd/elasticsearch/core/ResponseResoverTest.java
+++ xd-spring-boot-elasticsearch-starter/src/test/java/com/xd/elasticsearch/core/ResponseResoverTest.java
@@ -1,5 +1,6 @@
 package com.xd.elasticsearch.core;
 
+import com.xd.elasticsearch.core.response.JSONResponseResolver;
 import com.xd.elasticsearch.core.response.ResponseResover;
 import com.xd.elasticsearch.repository.bean.User;
 
@@ -8,7 +9,7 @@ import java.util.List;
 class ResponseResoverTest {
 
     public static void main(String[] args){
-        ResponseResover responseResover= new ResponseResover();
+        ResponseResover responseResover= new JSONResponseResolver();
         List<User> list= responseResover.resover("{\"columns\":[{\"name\":\"id\",\"type\":\"keyword\"},{\"name\":\"nickname\",\"type\":\"keyword\"}],\"rows\":[[\"20002\",\"test008\"]]}", User.class);
         System.out.println(list.get(0).getId());
     }
